678b6e2e32192d9b151c9281ee3d43391536543e
Derick Gross <derick.gross@gmail.com>
2020-04-11 20:56:41 -0400

    style config form

--- src/components/CustomServiceConfiguration/CustomServiceConfiguration.js
+++ src/components/CustomServiceConfiguration/CustomServiceConfiguration.js
@@ -6,7 +6,7 @@ const CustomServiceConfiguration = (props) => {
 	return (
 		<section>
 			<h2>Custom configurations</h2>
-			<p>Select a requesting service and responding service from the drop-down menus below.  If a custom configuration already exists for the pair, it will be displayed in the text input below.</p>
+			<p>Select a requesting service and responding service from the drop-down menus below.  If a custom configuration already exists for the pair, it will be displayed in the text field below.</p>
 			<ConfigForm {...props.configForm} displayConfigUpdateSuccess={props.displayConfigUpdateSuccess}/>
 		</section>
 	);
